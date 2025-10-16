57e0f0d0029926bb7a2042bfacf2dcdec0e61e05
ivcerveira <ivaldocerveira@outlook.com>
2025-10-16 07:38:26 -0300

    Add _Documentos.zip

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
         // Garante que a criação da GUI será feita na thread correta
         SwingUtilities.invokeLater(() -> {
             MyForm form = new MyForm(); // instanciando seu Form
